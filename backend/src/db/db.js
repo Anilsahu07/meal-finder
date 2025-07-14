5d75affe7d486056d61f96c929c7a5f5c1b6019c
Anilsahu07 <myhousehome1111@gmail.com>
2025-07-14 20:24:50 +0530

    changes

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -6,8 +6,7 @@ dotenv.config()
 
 const connect=async()=>{
     await mongoose.connect(process.env.MONGO_URI,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+        
     })
     .then(()=>{
         console.log("Database connected");
