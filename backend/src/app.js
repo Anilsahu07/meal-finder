ae1788eb90b3b3a419ac306c71081a8f4007f6d7
Anilsahu07 <myhousehome1111@gmail.com>
2025-07-14 20:18:01 +0530

    changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,8 +4,6 @@ const cors= require("cors")
 const cookieParser= require("cookie-parser")
 const userRouter= require("./routes/userRoutes")
 const mealRouter= require("./routes/mealRoutes")
-const dotenv= require("dotenv")
-dotenv.config()
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
